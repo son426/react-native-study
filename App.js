eca50ea3580bcabf560b0f74fd65f7acbac7db77
son426 <coghks0426@naver.com>
2023-09-26 01:57:28 +0900

    section4 스타일링 끝

--- App.js
+++ App.js
@@ -1,81 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-  FlatList,
-} from "react-native";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { StyleSheet, ImageBackground } from "react-native";
+
+import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  function toggleAddGoalHandler() {
-    setIsModalVisible((prev) => !prev);
-  }
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals((prev) => [
-      ...prev,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-    toggleAddGoalHandler();
-  }
-  function deleteGoalHandler(id) {
-    setCourseGoals((prev) => {
-      return prev.filter((goal) => goal.id !== id);
-    });
-  }
-
   return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.appContainer}>
-        <Button
-          title="goal 추가"
-          color="purple"
-          onPress={toggleAddGoalHandler}
-        />
-        <GoalInput
-          visible={isModalVisible}
-          onAddGoal={addGoalHandler}
-          onCancel={toggleAddGoalHandler}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  id={itemData.item.id}
-                  onDeleteItem={deleteGoalHandler}
-                />
-              );
-            }}
-            // 데이터안에 key가 없을 때는, 이렇게 keyExtractor 써서 key 설정할 수 있음.
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
-          ></FlatList>
-        </View>
-      </View>
-    </>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/lim.png")}
+        resizeMode="contain"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
+  rootScreen: {
     flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
   },
-  goalsContainer: {
-    flex: 5,
+  backgroundImage: {
+    opacity: 0.5,
   },
 });
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,61 @@
+import { View, TextInput, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Start</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#4e0329",
+    elevation: 6, // 안드로이드 전용
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.5,
+    alignItems: "center",
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    marginVertical: 8,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
