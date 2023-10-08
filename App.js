1de6c7e54833e0c12837d885000019959e1f4f3e
son426 <coghks0426@naver.com>
2023-10-08 14:46:59 +0900

    test

--- App.js
+++ App.js
@@ -1,65 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
-
-import StartGameScreen from "./screens/StartGameScreen";
-import GameScreen from "./screens/GameScreen";
-import { LinearGradient } from "expo-linear-gradient";
-import { useEffect, useState } from "react";
-import GameOverScreen from "./screens/GameOverScreen";
-import { useFonts } from "expo-font";
+import { StyleSheet, SafeAreaView } from "react-native";
+import CategoriesScreen from "./screens/CatagoriesScreen";
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [isGameOver, setIsGameOver] = useState(false);
-  const [guessRounds, setGuessRounds] = useState(0);
-
-  useFonts({});
-
-  function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber);
-    setIsGameOver(false);
-  }
-  function gameOverHandler(numberOfRounds) {
-    setIsGameOver(true);
-    setGuessRounds(numberOfRounds);
-  }
-  function startNewGameHandler() {
-    setUserNumber(null);
-    setGuessRounds(0);
-    setIsGameOver(false);
-  }
-
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
-
-  if (userNumber) {
-    screen = (
-      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-    );
-  }
-
-  if (isGameOver) {
-    screen = (
-      <GameOverScreen
-        userNumber={userNumber}
-        roundsNumber={guessRounds}
-        onRestart={startNewGameHandler}
-      />
-    );
-  }
-
   return (
     <>
       <StatusBar />
-      <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-        <ImageBackground
-          source={require("./assets/images/lim.png")}
-          resizeMode="contain"
-          style={styles.rootScreen}
-          imageStyle={styles.backgroundImage}
-        >
-          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-        </ImageBackground>
-      </LinearGradient>
+      <SafeAreaView style={styles.rootScreen}>
+        <CategoriesScreen />
+      </SafeAreaView>
     </>
   );
 }
@@ -67,8 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-  },
-  backgroundImage: {
-    opacity: 0.5,
   },
 });
